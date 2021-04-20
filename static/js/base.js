558aaf3a1a3c6de953ba584228342f30b9ced0e3
VAC <37587399+vacnex@users.noreply.github.com>
2021-04-21 01:31:29 +0700

    Re-write base template
    footer
    navbar

--- static/js/base.js
+++ static/js/base.js
@@ -1,12 +1,37 @@
 $(document).ready(function () {
-    $("#menu-toggle").click(function (e) {
-            e.preventDefault();
-            $("#wrapper").toggleClass("toggled");
-        });
-    $('.nav-link').hover(function () {
-            $(this).addClass('active');
-        }, function () {
-            $('.nav-link').removeClass('active');
+    var mobileTopBar = $(".s-top-container"),
+        mobileTopBarToggle = $("#mobileTopBarBtn, #mobileTopBarCloseBtn");
+
+    // mobileTopBarToggle.on("click", function(e) {
+    //     e.preventDefault();
+    //     mobileTopBar.toggleClass("show");
+    //     mobileTopBarToggle.toggleClass("hidden");
+    // });
+    mobileTopBarToggle.on("click", function(e) {
+        e.preventDefault();
+        console.log('test')
+        if ($(mobileTopBarToggle[0]).attr('id') == 'mobileTopBarBtn') {
+            $('#'+$(mobileTopBarToggle[0]).attr('id')).prop('id', 'mobileTopBarCloseBtn');
+            $('#'+$(mobileTopBarToggle[0]).attr('id')).prop('class', 'fas fa-times');
+            mobileTopBar.toggleClass("show");
         }
-    );
+        else if ($(mobileTopBarToggle[0]).attr('id') =='mobileTopBarCloseBtn') {
+            $('#'+$(mobileTopBarToggle[0]).attr('id')).prop('id', 'mobileTopBarBtn');
+            $('#'+$(mobileTopBarToggle[0]).attr('id')).prop('class', 'far fa-user-circle');
+            mobileTopBar.removeClass("show");
+        }
+        
+    });
+
+    var sidebar = $("#nav-drawer"),
+        sidebarToggle = $(".sidebar-btn, .sidebar-btn--close");
+        pageWrapper = $("#page-wrapper, #page-footer .c-container");
+        topBarWrapper = $(".s-top");
+
+    sidebarToggle.on("click", function(e) {
+        e.preventDefault();
+        sidebar.toggleClass("closed show-rwd");
+        pageWrapper.toggleClass("l-with-sidebar");
+        topBarWrapper.toggleClass("s-top--sidebar");
+    });
 });

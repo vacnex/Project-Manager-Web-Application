9cabb97b0fe01b83ee2ea8df5ec3e6f3d24a9066
VAC <37587399+vacnex@users.noreply.github.com>
2021-05-15 22:57:48 +0700

    Update base.js
    add more res

--- static/js/base.js
+++ static/js/base.js
@@ -26,82 +26,130 @@ $(document).ready(function () {
         pageWrapper.toggleClass('l-with-sidebar');
     });
     var breakpoint = {
-      sm: 576,
-      md: 768,
-      lg: 992,
-      xl: 1200,
+        sm: 576,
+        md: 768,
+        lg: 992,
+        xl: 1200,
+        xxl: 1400,
+        exxxl: 1600,
+        xxxl: 1900,
+        xxxxl: 2500,
     };
       
     $('.course-list-content').slick({
-      mobileFirst: true,
-      slidesToShow: 1,
-      
-      arrows: true,
-      nextArrow: '#btn-next',
-      prevArrow: '#btn-prev',
-      responsive: [
-        {
-          breakpoint: breakpoint.sm,
-          settings: {
-            slidesToShow: 2,
-            
-          },
-        },
-        {
-          breakpoint: breakpoint.md,
-          settings: {
-            slidesToShow: 3,
-            
-          },
-        },
-        {
-          breakpoint: breakpoint.lg,
-          settings: {
-            slidesToShow: 4,
-            
-          },
-        },
-        {
-          breakpoint: breakpoint.xl,
-          settings: {
-            slidesToShow: 5,
-            
-          },
-        },
-      ],
+        mobileFirst: true,
+        slidesToShow: 1,
+        arrows: true,
+        nextArrow: '#btn-next',
+        prevArrow: '#btn-prev',
+        responsive: [
+            {
+                breakpoint: breakpoint.sm,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+            {
+                breakpoint: breakpoint.md,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+            {
+                breakpoint: breakpoint.lg,
+                settings: {
+                    slidesToShow: 3,
+                },
+            },
+            {
+                breakpoint: breakpoint.xl,
+                settings: {
+                    slidesToShow: 3,
+                },
+            },
+            {
+                breakpoint: breakpoint.xxl,
+                settings: {
+                    slidesToShow: 3,
+                },
+            },
+            {
+                breakpoint: breakpoint.exxxl,
+                settings: {
+                    slidesToShow: 4,
+                },
+            },
+
+            {
+                breakpoint: breakpoint.xxxl,
+                settings: {
+                    slidesToShow: 5,
+                },
+            },
+            {
+                breakpoint: breakpoint.xxxxl,
+                settings: {
+                    slidesToShow: 6,
+                },
+            },
+        ],
     });
     $('.project-list-content').slick({
-      mobileFirst: true,
-      slidesToShow: 1,
+        mobileFirst: true,
+        slidesToShow: 1,
+        arrows: true,
+        nextArrow: '#btn-next',
+        prevArrow: '#btn-prev',
+        responsive: [
+            {
+                breakpoint: breakpoint.sm,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+            {
+                breakpoint: breakpoint.md,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+            {
+                breakpoint: breakpoint.lg,
+                settings: {
+                    slidesToShow: 3,
+                },
+            },
+            {
+                breakpoint: breakpoint.xl,
+                settings: {
+                    slidesToShow: 3,
+                },
+            },
+            {
+                breakpoint: breakpoint.xxl,
+                settings: {
+                    slidesToShow: 3,
+                },
+            },
+            {
+                breakpoint: breakpoint.exxxl,
+                settings: {
+                    slidesToShow: 4,
+                },
+            },
 
-      arrows: true,
-      nextArrow: '#btn-next',
-      prevArrow: '#btn-prev',
-      responsive: [
-        {
-          breakpoint: breakpoint.sm,
-          settings: {
-            slidesToShow: 2,
-          },
-        },
-        {
-          breakpoint: breakpoint.md,
-          settings: {
-            slidesToShow: 3,
-          },
-        },
-        {
-          breakpoint: breakpoint.lg,
-          settings: {
-            slidesToShow: 4,
-          },
-        },
-        {
-          breakpoint: breakpoint.xl,
-          settings: {
-            slidesToShow: 5,
-          },
-        },
-      ],
+            {
+                breakpoint: breakpoint.xxxl,
+                settings: {
+                    slidesToShow: 5,
+                },
+            },
+            {
+                breakpoint: breakpoint.xxxxl,
+                settings: {
+                    slidesToShow: 6,
+                },
+            },
+        ],
     });
 });

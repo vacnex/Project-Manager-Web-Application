2c4b9a0a0910554af549fdb92d29290ee122215f
VAC <37587399+vacnex@users.noreply.github.com>
2021-11-11 23:24:15 +0700

    improve UI

--- static/js/Home/home-manager.js
+++ static/js/Home/home-manager.js
@@ -1,12 +1,2 @@
 $(document).ready(function () {
-  $(document).on('click', '#pills-gv-tab', function () {
-    $(this).removeClass('active');
-    $(this).addClass('btn-custom');
-    $('#pills-sv-tab').removeClass('btn-custom');
-  });
-  $(document).on('click', '#pills-sv-tab', function () {
-    $(this).removeClass('active');
-    $(this).addClass('btn-custom');
-    $('#pills-gv-tab').removeClass('btn-custom');
-  });
 });
